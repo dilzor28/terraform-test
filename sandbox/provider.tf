42e757b1ffc5dc55cdbf22efa2ede0b0e6983bf9
dilzor28 <dillon@launchzero.com>
2022-11-10 09:15:10 -0500

    initializing repo

---
+++ sandbox/provider.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "aws_account_ids" {
+  type    = list
+  default = null
+}
+
+terraform {
+  required_version = ">= 0.12.20"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 2.46.0"
+    }
+  }
+}
+
+provider "aws" {
+  region              = var.aws_region
+  allowed_account_ids = var.aws_account_ids
+  profile             = "default"
+}
